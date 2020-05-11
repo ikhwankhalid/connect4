22fca201c6f84d476f4d7308a094c97975ab8c30
ikhwankhalid <ikhwankhalid92@gmail.com>
2020-05-12 01:58:35 +0200

    Added preliminary row scoring

--- agent00/connectn/agent_minimax/minimax.py
+++ agent00/connectn/agent_minimax/minimax.py
@@ -1,12 +1,33 @@
-from connectn.common import PLAYER1, PLAYER2, GameState, BoardPiece
+from connectn.common import PLAYER1, PLAYER2, NO_PLAYER, GameState, BoardPiece
 from connectn.common import connected_four
 import numpy as np
 
 
-def scoring(board: np.ndarray, player: BoardPiece) -> int:
+def position_scoring(board: np.ndarray, player: BoardPiece) -> int:
     """
-        Should evaluate the board and return scores for playing
-        empty positions
+        Should evaluate the board and return scores for possible moves
+        TODO: Column scoring
+    """
+    score = 0
+    for r in board.shape[0]:
+        r_window = [i for i in board[r, :]]
+        for c in board.shape[1] - 3:
+            c_window = r_window[c:c + 4]
+            if c_window.count(player) == 4:
+                score += 1000
+            elif c_window.count(player) == 3 and c_window.count(NO_PLAYER) == 1:
+                score += 100
+            elif c_window.count(player) == 2 and c_window.count(NO_PLAYER) == 2:
+                score += 10
+            elif c_window.count(player) == 1 and c_window.count(NO_PLAYER) == 3:
+                score += 1
+    return score
+
+
+def best_move(board: np.ndarray, player: BoardPiece):
+    """
+        Should find the best column to play by applying actions then
+        computing the new score
     """
     pass
 
